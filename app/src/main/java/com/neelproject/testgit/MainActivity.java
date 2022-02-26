2a0b51affca93176efdd5341a494d862bc8573bf
Raghavendrakaiv <raghavendrakaiv@gmail.com>
2022-02-26 22:01:09 +0530

    new commit

--- app/src/main/java/com/neelproject/testgit/MainActivity.java
+++ app/src/main/java/com/neelproject/testgit/MainActivity.java
@@ -3,6 +3,10 @@ package com.neelproject.testgit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void print(View view) {
+        TextView t = findViewById(R.id.textView);
+        Button b = findViewById(R.id.button);
+        EditText e = findViewById(R.id.Text);
+        Button d = findViewById(R.id.delete);
+
+
+        String r = "Hello Raghu";
+        t.setText(e.getText().toString());
+
+    }
+
+
+    public void delete(View view) {
+        TextView t = findViewById(R.id.textView);
+        t.setText("");
+
     }
 }
